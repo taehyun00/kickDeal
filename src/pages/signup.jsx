7494306964dace3040c0ee4b564e5b391a96eaf4
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-02-21 22:59:43 +0900

    error fix and 1st signup page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -2,14 +2,48 @@ import '../pagescss/signup.css';
 import React from 'react'; 
 
 
+
 export default function Signup() {
   
   return (
-      <div class="main">
+      <div className="Ma">
         <div className='forms'>
+          <div className='ft'>본인정보를 입력해주세요</div>
 
+          <div className='realforms'>
+            <div className='divW'>이름</div>
+              <div className='fi'>
+                <input  className='fin' placeholder='홍길동'/>
+              </div>
+
+
+
+
+            <div className='divW'>생년월일</div>
+              <div className='fi'>
+                <input  className='fin' placeholder='2000.11.17'/>
+              </div>
+
+
+
+
+            
+            <div className='divW'>이메일</div>
+              <div className='fi'>
+                <input  className='fin' placeholder='tes1234@gmail.com'/>
+              </div> 
+            
+            <div className='bt'>
+              <button className='btt'>다음</button>
+
+            </div>
+            
+            
+            </div>
+          </div>
         </div>
-    </div>
+
+
 
   );
 }
