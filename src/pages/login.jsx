939d0bc2465f88e8304304ee7c0c942aa2f26da6
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-02-26 15:12:50 +0900

    login #2 이건우 이새끼 데이트한다고 하 서버 주소 안넘겨줌

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,34 @@
 import '../pagescss/login.css';
 import logo from '../images/logo.svg'
+import React, {useState} from 'react'; 
+import axios from 'axios';
 
 export default function Login() {
+
+  const [Pw,setPw] = useState("");
+  const [Id,setId] = useState("");
+
+  const requestAccessToken = async (Id, Pw) => {
+    try {
+      const response = await axios.post(`${serverURL}/login`, {
+        id: Id,
+        password: Pw,
+      });
+
+      const accessToken = response.data.access;
+      
+      if (accessToken) {
+        // ✅ 토큰 저장 (localStorage 또는 axios 헤더 설정)
+        localStorage.setItem('accessToken', accessToken);
+        axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+        
+        console.log("로그인 성공! 토큰 저장 완료:", accessToken);
+      }
+    } catch (e) {
+      console.error("로그인 실패:", e.response?.data || e.message);
+    }
+  };
+  
 
   return (
       <div className="Ma_">
@@ -16,17 +43,21 @@ export default function Login() {
 
             <div className='divW'>아이디</div>
               <div className='fi'>
-                <input  className='fin' placeholder='아이디를 입력해주세요'/>
+                <input  className='fin' placeholder='아이디를 입력해주세요' value ={Id} onChange={(event)=> 
+                  {setId(event.target.value);
+                  }}/>
               </div> 
 
              <div className='divW'>비밀번호</div>
               <div className='fi'>
-                <input  className='fin' placeholder='비밀번호를 입력해주세요'/>
+                <input  className='fin' placeholder='비밀번호를 입력해주세요' value={Pw}onChange={(event)=> 
+                  {setPw(event.target.value);
+                  }}/>
               </div> 
             
             <hr className='line'></hr>
             <div className='bt_'>
-              <button className='btt_'>로그인</button>
+              <button className='btt_' onClick={() => requestAccessToken(Id,Pw)}>로그인</button>
 
             </div>
             
